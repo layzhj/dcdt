33ea38558b7b0bbdf43fcfb80d86480a3d419843
layzhj <layzhj@gmail.com>
2025-04-07 21:01:18 +0800

    Update dcdt.py

--- dcdt.py
+++ dcdt.py
@@ -1,16 +1,23 @@
-from neuron import h
-import numpy as np
+from neuron import h, gui
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
+from datetime import *
+import sys
 rcParams['font.family'] = 'SimHei'
 plt.rcParams['axes.unicode_minus'] =False
+# sys.setrecursionlimit(30000)
+current_time = datetime.now()
+print(current_time)
 
-
-h.load_file('stdgui.hoc')
+h.load_file('stdrun.hoc')
 h.cvode_active(1)
 cv = h.CVode()
-
-# 
+cv.atolscale("DcDt.ng", 1e-22)
+cv.atolscale("DcDt.U", 1)
+cv.atolscale("DcDt.Z", 1e-6)
+cv.jacobian(2)
+# cv.maxorder(12)
+cv.stiff(0)
 
 A = 300e3
 f = 500e3
@@ -28,16 +35,19 @@ stim.z0 = 6.3168827639957465e-6
 
 
 stim.tbegin = 100
-stim.tdur = 100
-t_vec = h.Vector().record(h._ref_t)
-c_vec = h.Vector().record(soma(0.5)._ref_cm)
-z_vec = h.Vector().record(stim._ref_U)
-v_vec = h.Vector().record(soma(0.5)._ref_v)
+stim.tdur = 1000
+t_vec = h.Vector().record(h._ref_t, 0.1)
+c_vec = h.Vector().record(soma(0.5)._ref_cm, 0.1)
+z_vec = h.Vector().record(stim._ref_U, 0.1)
+v_vec = h.Vector().record(soma(0.5)._ref_v, 0.1)
 
-h.tstop=300
-# h.dt = 0.025
+h.tstop=1500
+# h.dt = 1e-6
 h.finitialize(-65)
 h.run()
+
+end_time = datetime.now()
+print(end_time)
 
 t_array = t_vec.as_numpy()
 z_array = z_vec.as_numpy()
