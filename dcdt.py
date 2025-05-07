4d74f2e0a1ca9623704661744126e542b34aaefe
layzhj <layzhj@gmail.com>
2025-05-07 16:08:18 +0800

    Update dcdt.py

--- dcdt.py
+++ dcdt.py
@@ -2,12 +2,14 @@ from neuron import h
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
 import time
+import pandas as pd
 
 rcParams['font.family'] = 'SimHei'
 plt.rcParams['axes.unicode_minus'] =False
 
 start_time = time.time()
 
+save_path = './hoc_long.csv'
 
 h.load_file('stdgui.hoc')
 h.cvode_active(1)
@@ -15,7 +17,7 @@ cv = h.CVode()
 cv.atolscale("DcDt.ng", 1e-22)
 cv.atolscale("DcDt.U", 1)
 cv.atolscale("DcDt.Z", 1e-6)
-cv.atol(1e-10)
+# cv.atol(1e-10)
 
 
 A = 300e3
@@ -34,16 +36,23 @@ stim.z0 = 6.3168827639957465e-6
 stim.rel_Zmin = -0.49
 
 stim.tbegin = 100
-stim.tdur = 100
-t_vec = h.Vector().record(h._ref_t)
-c_vec = h.Vector().record(soma(0.5)._ref_cm)
-z_vec = h.Vector().record(stim._ref_Z)
-ng_vec = h.Vector().record(stim._ref_ng)
-q_vec = h.Vector().record(stim._ref_q)
-stm = h.Vector().record(stim._ref_stm)
-U_vec = h.Vector().record(stim._ref_U)
+stim.tdur = 50
+# t_vec = h.Vector().record(h._ref_t)
+# c_vec = h.Vector().record(soma(0.5)._ref_cm)
+# z_vec = h.Vector().record(stim._ref_Z)
+# ng_vec = h.Vector().record(stim._ref_ng)
+# q_vec = h.Vector().record(stim._ref_q)
+# stm = h.Vector().record(stim._ref_stm)
+# U_vec = h.Vector().record(stim._ref_U)
+t_vec = h.Vector().record(h._ref_t, 0.1)
+c_vec = h.Vector().record(soma(0.5)._ref_cm, 0.1)
+z_vec = h.Vector().record(stim._ref_Z, 0.1)
+ng_vec = h.Vector().record(stim._ref_ng, 0.1)
+q_vec = h.Vector().record(stim._ref_q, 0.1)
+stm = h.Vector().record(stim._ref_stm, 0.1)
+U_vec = h.Vector().record(stim._ref_U, 0.1)
 
-h.tstop=300
+h.tstop=150
 # h.dt = 1e-6
 h.finitialize(-65)
 try:
@@ -63,6 +72,8 @@ stm_array = stm.as_numpy()
 U_array = U_vec.as_numpy()
 print(z_array)
 
+# df = pd.DataFrame({'t': t_array, 'z': z_array, 'ng': ng_array, 'q': q_array, 'stm': stm_array, 'U': U_array, 'c': c_array})
+# df.to_csv(save_path, index=False)
 
 fig, ax = plt.subplots(6, 1)
 
